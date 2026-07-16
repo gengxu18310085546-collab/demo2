13e9e99c9b0def4b3946187725ef442c979de845
gengxu18310085546-collab <gengxu18310085546@163.com>
2026-07-16 16:24:11 +0800

    Modify delay and countdown for message bombardment
    Increased delay before starting the message bombardment and adjusted countdown.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ keyboard=Controller()
 messages=input("请输入你要轰炸的信息：")
 times=eval(input("请输入你要轰炸的次数："))
 print("数据已被后台接受，请将光标移动至会话框")
-time.sleep(2)
-for i in range(3):
-    print("距离信息轰炸还需要%d秒"%(3-i))
+time.sleep(5)
+for i in range(5):
+    print("距离信息轰炸还需要%d秒"%(5-i))
     time.sleep(1)
 for i in range(times):
     keyboard.type(messages)
